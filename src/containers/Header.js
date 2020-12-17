8e14ed33436d7aa3606a5ef1d9f4df00c22c6c81
Philip-Lau <philip-Lau@noreply.github.com>
2020-12-17 14:11:09 +0800

    add header animation

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,18 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => (
-  <header className="main-header">
-    <div className="main-header__wrapper">
-      <Link className="main-header__logo" to="/">
-        Philarty
-      </Link>
-      <nav className="main-header__links">
-        <Link to="/about">About</Link>
-        <Link to="/">Projects</Link>
-      </nav>
-    </div>
-  </header>
-);
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      scroll: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    this.setState({ scroll: window.scrollY > 0 });
+  };
+
+  render() {
+    const { scroll } = this.state;
+    return (
+      <React.Fragment>
+        <header
+          style={{ transition: scroll ? "all 0.25s ease-in-out" : "" }}
+          className={"main-header" + (scroll ? " main-header--scroll" : "")}
+        >
+          <div className="main-header__wrapper">
+            <Link
+              className="main-header__logo"
+              to="/"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              Philarty
+            </Link>
+            <nav className="main-header__links">
+              <Link to="/about">About</Link>
+              <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+                Projects
+              </Link>
+            </nav>
+          </div>
+        </header>
+
+        <div className="pseudo-header" />
+      </React.Fragment>
+    );
+  }
+}
 
 export default Header;
