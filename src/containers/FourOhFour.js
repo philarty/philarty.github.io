089d7b36cb167074bd02045b0a6c53b483cbdd10
Philip-Lau <philip-Lau@noreply.github.com>
2020-12-20 09:38:49 +0800

    update styling

--- src/containers/FourOhFour.js
+++ src/containers/FourOhFour.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const FourOhFour = () => {
   return (
-    <section>
+    <section className="404">
       <h1>AIYA</h1>
+      <h3>Seems like you are lost.</h3>
+      <Link to="/">
+        <h3>Lets head home</h3>
+      </Link>
     </section>
   );
 };
