281acef760cabb4ff802ec34607ee3de63aa1847
Philip-Lau <philip-Lau@noreply.github.com>
2020-12-22 17:47:33 +0800

    relocate
    relocate PROJECTS const

--- src/projects/PROJECTS.js
+++ src/projects/PROJECTS.js
@@ -1,51 +1,51 @@
 const PROJECTS = [
   {
     title: "Design Project",
+    id: "test1",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also ",
     imageSrc: "https://picsum.photos/seed/1/500/300",
     tags: ["design"],
-    url: "test1",
   },
   {
     title: "Code Project",
+    id: "test2",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also ",
     imageSrc: "https://picsum.photos/seed/2/500/300",
     tags: ["code"],
-    url: "test2",
   },
   {
     title: "Make Project",
+    id: "test3",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also ",
     imageSrc: "https://picsum.photos/seed/3/500/300",
     tags: ["make"],
-    url: "test3",
   },
   {
     title: "Design Project 2",
+    id: "test4",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also ",
     imageSrc: "https://picsum.photos/seed/4/500/300",
     tags: ["design", "make"],
-    url: "test1",
   },
   {
     title: "Code Project 3",
+    id: "test5",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also ",
     imageSrc: "https://picsum.photos/seed/5/500/300",
     tags: ["code", "design"],
-    url: "test2",
   },
   {
     title: "Make Project2",
+    id: "test6",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also ",
     imageSrc: "https://picsum.photos/seed/6/500/300",
     tags: ["make", "code"],
-    url: "test3",
   },
 ];
 
