e8f37c6f85a21368a93fd953670a80308b755ece
Philip-Lau <philip-Lau@noreply.github.com>
2020-12-22 17:01:43 +0800

    updat 404
    update 404

--- src/containers/FourOhFour.js
+++ src/containers/FourOhFour.js
@@ -6,11 +6,19 @@ const FourOhFour = () => {
     <section className="four-oh-four">
       <h1>AIYA</h1>
       <h3>Seems like you are lost.</h3>
-      <Link to="/" className='btn btn-lg btn-solid-red'>
-        Lets head home
+      <img src='https://source.unsplash.com/600x600?cute-animals' alt='Cute Animals' />
+      <p>here are some cute animals for your troubles</p>
+      <div>
+      <a href='http://philarty.github.io/portfolio#/404' className='btn btn-lg btn-solid-red' >
+        I want more cutness
+      </a>
+      <Link to="/" className='btn btn-lg btn-solid-green'>
+        Go back to Portfolio
       </Link>
+
+      </div>
     </section>
   );
 };
-
+ 
 export default FourOhFour;
