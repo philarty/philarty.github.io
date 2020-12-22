5e29ef9b34abd1978280184d8105c6223b7ebcff
Philip-Lau <philip-Lau@noreply.github.com>
2020-12-22 18:11:51 +0800

    unnest link within link.
    unnest link within link.

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -5,23 +5,22 @@ const ProjectPreview = (props) => {
   const { project } = props;
   return (
     <div className="project-preview">
+      <Link to={"/project/" + project.id} className="project-preview__link" />
       <div className="project-preview__img">
         <img src={project.imageSrc} alt={project.title} />
       </div>
-      <div className="project-preview__text">
-        <h3>{project.title}</h3>
-        <p>{project.description}</p>
-        <div className="project-preview__tags">
-          {project.tags.map((tag) => (
-            <Link
-              to={"/projects/" + tag}
-              key={tag}
-              onClick={() => window.scrollTo(0, 0)}
-            >
-              {tag}
-            </Link>
-          ))}
-        </div>
+      <h3>{project.title}</h3>
+      <p>{project.description}</p>
+      <div className="project-preview__tags">
+        {project.tags.map((tag) => (
+          <Link
+            to={"/projects/" + tag}
+            key={tag}
+            onClick={() => window.scrollTo(0, 0)}
+          >
+            {tag}
+          </Link>
+        ))}
       </div>
     </div>
   );
