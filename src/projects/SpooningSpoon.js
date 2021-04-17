996e5b009dd24de9fc34f3e682a1d6dc01a0e522
Philip-Lau <philip-Lau@noreply.github.com>
2021-04-17 15:22:00 -0700

    link images

--- src/projects/SpooningSpoon.js
+++ src/projects/SpooningSpoon.js
@@ -1,6 +1,45 @@
+import React from "react";
+import Lightbox from "../components/Lighbox";
+import spooningSpoonPoster from "../images/spooning-spoon/spooning-spoon-poster.jpg";
+import spooningSpoonDefinition from "../images/spooning-spoon/spooning-spoon-definition.png";
+import spooningSpoonProcess from "../images/spooning-spoon/spooning-spoon-process.png";
+import spooningSpoonCover from "../images/spooning-spoon/spooning-spoon-cover.jpg";
+import spooningSpoonPhil from "../images/spooning-spoon/spooning-spoon-phil.jpg";
+
 const SpooningSpoon = (
   <div className="project__body">
-    <div>test page! woot. this is something... i guess.</div>
+    <Lightbox>
+      <img src={spooningSpoonPoster} alt="" />
+    </Lightbox>
+    <Lightbox>
+      <img src={spooningSpoonDefinition} alt="" />
+    </Lightbox>{" "}
+    <div>
+      <h3>
+        Spoon <span>\spoon\</span>
+      </h3>
+      <ol>
+        <h4>noun</h4>
+        <li>
+          An implement consisting of a small, shallow oval or round bowl on a
+          long handle, used for eating, stirring, and serving food.
+        </li>
+        <h4>verb</h4>
+        <li>
+          (of two people) lie close together sideways and front to back with
+          bent knees, so as to fit together like spoons.
+        </li>
+      </ol>
+    </div>
+    <Lightbox>
+      <img src={spooningSpoonProcess} alt="" />
+    </Lightbox>
+    <Lightbox>
+      <img src={spooningSpoonCover} alt="" />
+    </Lightbox>
+    <Lightbox>
+      <img src={spooningSpoonPhil} alt="" />
+    </Lightbox>
   </div>
 );
 
