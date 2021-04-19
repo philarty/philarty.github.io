314d3cb590218c2b9c7dae23a767e5c7a38c430d
Philip-Lau <philip-Lau@noreply.github.com>
2021-04-18 21:19:25 -0700

    add underconstruction - warning

--- src/containers/Project.js
+++ src/containers/Project.js
@@ -31,6 +31,16 @@ const Project = () => {
   if (project) {
     return (
       <section className="project">
+        {project.hidden && (
+          <div className="project__section text-center">
+            <h2>Under Construction</h2>
+            <p>
+              Seems like you wondered into this page that is under construction.
+              Feel free to explore around, but please excuse the mess. and if
+              you have any feedback, send it over! Always appreciate it!
+            </p>
+          </div>
+        )}
         {/* <div className="project__header">
           <h2>{project.title}</h2>
           {project.summary.map((sum) => (
