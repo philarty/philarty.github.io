6ecc973c931243d7e9992ae55ac4810c48ee1c5a
Philip-Lau <philip-Lau@noreply.github.com>
2021-05-18 11:50:51 -0700

    add color styling for toggle switch

--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,8 +1,14 @@
 const ToggleSwitch = (props) => {
-  const { onToggle, isSelected, children } = props;
+  const { onToggle, btnColor, isSelected, children } = props;
 
   return (
-    <label className={(isSelected ? "checked " : " ") + "toggle-switch"}>
+    <label
+      className={
+        "toggle-switch" +
+        (btnColor ? " toggle-switch--" + btnColor : " toggle-switch--default") +
+        (isSelected ? " checked " : "")
+      }
+    >
       <input type="checkbox" onClick={onToggle} checked={isSelected} />
       <span className="toggle-switch__slider" />
       {children && <span className="toggle-switch__label">{children}</span>}
