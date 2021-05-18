e23f08388a7314930ad3e4cc56499a8080014f1e
Philip-Lau <philip-Lau@noreply.github.com>
2021-05-18 11:50:32 -0700

    create button select

---
+++ src/components/ButtonSelect.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Button from "./Button";
+
+const ButtonSelect = (props) => {
+  const {
+    options,
+    btnSize,
+    btnColor,
+    block,
+    value,
+    onSelect,
+    className,
+    ...rest
+  } = props;
+
+  return (
+    <div className={"btn-group" + (className ? " " + className : "")}>
+      {options.map((option) => (
+        <Button
+          btnSize={btnSize}
+          btnStyle={value === option.value ? "solid" : "outline"}
+          btnColor={btnColor}
+          block={block}
+          onClick={() => onSelect(option.value)}
+          {...rest}
+        >
+          {option.label}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default ButtonSelect;
