198197e604605a27a741f5d4b2288e296f30002d
Philip-Lau <philip-Lau@noreply.github.com>
2021-05-26 23:09:07 -0700

    add link and a

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Button = React.forwardRef((props, ref) => {
   const {
@@ -9,27 +10,39 @@ const Button = React.forwardRef((props, ref) => {
     icon,
     children,
     onClick,
+    to,
+    href,
     className,
     ...rest
   } = props;
-  return (
-    <button
-      ref={ref}
-      className={
-        "btn" +
-        (btnSize ? " btn-" + btnSize : " btn-md") +
-        (icon ? " btn-icon" : "") +
-        (btnStyle ? " btn-" + btnStyle : " btn-solid") +
-        (btnColor ? "-" + btnColor : "-default") +
-        (block ? " btn-block" : "") +
-        (className ? " " + className : "")
-      }
-      onClick={onClick}
-      {...rest}
-    >
-      {children}
-    </button>
-  );
+
+  const classNameString =
+    "btn" +
+    (btnSize ? " btn-" + btnSize : " btn-md") +
+    (icon ? " btn-icon" : "") +
+    (btnStyle ? " btn-" + btnStyle : " btn-solid") +
+    (btnColor ? "-" + btnColor : "-default") +
+    (block ? " btn-block" : "") +
+    (className ? " " + className : "");
+
+  if (href) {
+    return (
+      <a ref={ref} href={href} className={classNameString} {...rest}>
+        {children}
+      </a>
+    );
+  } else if (to) {
+    return (
+      <Link ref={ref} to={to} className={classNameString} {...rest}>
+        {children}
+      </Link>
+    );
+  } else
+    return (
+      <button ref={ref} className={classNameString} onClick={onClick} {...rest}>
+        {children}
+      </button>
+    );
 });
 
 export default Button;
