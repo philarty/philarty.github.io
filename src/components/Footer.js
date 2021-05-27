39cad469ada7c343290e5e5650fa662ece9417e0
Philip-Lau <philip-Lau@noreply.github.com>
2021-05-27 09:42:04 -0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div  className='footer__wrapper'>
-      <p>thanks for checking out my site! bye! </p>
+    <footer className="footer">
+      <div className="footer__wrapper">
+        <Link to="/" className="footer__logo">
+          &copy; Philarty 2018-2021
+        </Link>
+        <div className="footer__social">
+          <Link to="/" className="footer__logo">
+            Portfolio
+          </Link>
+          <a href="https://www.instagram.com/philarty/">Instagram</a>
+          <a href="https://www.linkedin.com/in/philiplaudesign/">linkedIn</a>
+        </div>
       </div>
     </footer>
   );
