3b6a21dbb65a6a68a94c869e4331f502d2799c33
Philip-Lau <philip-Lau@noreply.github.com>
2021-05-31 12:45:16 -0700

    Update ProjectLayout.js

--- src/components/ProjectLayout.js
+++ src/components/ProjectLayout.js
@@ -1,1 +1,37 @@
 import React from "react";
+
+const ProjectLayout = ({ children, className }) => {
+  let classNameString = "project";
+
+  if (className) {
+    classNameString = classNameString.concat(" " + className);
+  }
+  return <div className={classNameString}>{children}</div>;
+};
+
+const Header = ({ children, className }) => {
+  let classNameString = "project-header";
+
+  if (className) {
+    classNameString = classNameString.concat(" " + className);
+  }
+  return <div className={classNameString}>{children}</div>;
+};
+
+const Section = ({ children, className, id }) => {
+  let classNameString = "project-section";
+
+  if (className) {
+    classNameString = classNameString.concat(" " + className);
+  }
+  return (
+    <div className={classNameString} id={id}>
+      {children}
+    </div>
+  );
+};
+
+ProjectLayout.Header = Header;
+ProjectLayout.Section = Section;
+
+export default ProjectLayout;
