775b6daf099ba5418c1de12e37faa23d9851bced
Philip-Lau <philip-Lau@noreply.github.com>
2021-05-31 12:00:47 -0700

    update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Button from "./Button";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__wrapper">
-        <div>
-          &copy; Philarty 2018-2021
-        </div>
-        <div className="footer__social">
-          <Button to="/" btnStyle="link" btnColor="primary">
-            Portfolio
-          </Button>
-          <Button
-            href="https://www.instagram.com/philarty/"
-            btnStyle="link"
-            btnColor="primary"
-          >
-            Instagram
-          </Button>
-          <Button
-            href="https://www.linkedin.com/in/philiplaudesign/"
-            btnStyle="link"
-            btnColor="primary"
-          >
-            linkedIn
-          </Button>
-        </div>
+      <div className="footer__social">
+        <Button to="/" btnStyle="link" btnColor="primary" target="_blank">
+          Portfolio
+        </Button>
+        <Button
+          href="https://www.instagram.com/philarty/"
+          btnStyle="link"
+          btnColor="primary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </Button>
+        <Button
+          href="https://www.linkedin.com/in/philiplaudesign/"
+          btnStyle="link"
+          btnColor="primary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          linkedIn
+        </Button>
+        <Button
+          href="https://github.com/philarty"
+          btnStyle="link"
+          btnColor="primary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </Button>
+      </div>
+      <div className="footer__disclaimer">
+        &copy; 2021 All rights reserved by Philip Lau
       </div>
     </footer>
   );
