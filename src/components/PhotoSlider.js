3346171e948e7feb264da6d105a88411b1072789
Philip-Lau <philip-Lau@noreply.github.com>
2021-08-06 11:00:29 -0700

    photoSlider

---
+++ src/components/PhotoSlider.js
@@ -0,0 +1,59 @@
+import React, { useState, useRef, useEffect } from "react";
+
+export const useContainerWidth = (myRef) => {
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(myRef.current.offsetWidth);
+    };
+
+    if (myRef.current) {
+      setWidth(myRef.current.offsetWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [myRef]);
+
+  return width;
+};
+
+const PhotoSlider = (props) => {
+  const { leftSrc, RightSrc } = props;
+  const ref = useRef();
+  const width = useContainerWidth(ref);
+
+  const [sliderPosition, setSliderPosition] = useState(50);
+
+  return (
+    <div className="photo-slider" ref={ref}>
+      <div className="photo-slider__right">
+        <img src={RightSrc} style={{ width: width }} />
+      </div>
+      <div
+        className="photo-slider__left"
+        style={{ width: sliderPosition + "%" }}
+      >
+        <img src={leftSrc} style={{ width: width }} />
+      </div>
+      <input
+        className="photo-slider__slider"
+        type="range"
+        id="imgPosition"
+        name="imgPosition"
+        min={0}
+        max={100}
+        onChange={(e) => {
+          const value = parseInt(e.target.value, 10);
+          setSliderPosition(value);
+        }}
+      />
+    </div>
+  );
+};
+
+export default PhotoSlider;
