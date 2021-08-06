5d8002245a34ac4a9188c4f341946868a04e3881
Philip-Lau <philip-Lau@noreply.github.com>
2021-08-06 09:11:02 -0700

    migrate from collapsible

--- src/components/ResumeSection.js
+++ src/components/ResumeSection.js
@@ -12,8 +12,6 @@ const RESUME = [
         sub_header: "Rhode Island School of Design - RISD",
         title: "BFA Industrial Design",
         period: "2014 - 2018",
-        //   title: "Honor title Student",
-        //  period: 3.9,
       },
       {
         sub_header: "Brown University",
@@ -34,19 +32,17 @@ const RESUME = [
         sub_header: "Moesif",
         title: "UIUX Engineer",
         period: "2019 - Present",
-        collapsible: true,
         description: [
-          "Design UI/UX wireframes and build react components for both front facing website, and web application",
-          "Revamp and maintain static blog/docs site by building a cohesive Jekyll template in Liquid.",
-          "Develop and grow a consistent design system used across the site and application as well as branding guidelines",
+          "Improve user integration rates and reduce codebase by designing and refactoring react components for both front facing website, and web application",
+          "Redesign to a more user friendly design and maintain static blog/docs site by building a cohesive Jekyll template in Liquid.",
+          "Develop and grow a consistent design system and brand guidelines to be used across the site and application",
           "Create inclusive marketing material used across various social media including Youtube, Facebook, Reddit, LinkedIn",
         ],
       },
       {
         sub_header: "Praise HK",
         title: "UIUX Designer",
-        period: "2018",
-        collapsible: true,
+        period: "2018  - Present",
         description: [
           "Commissioned by HKUST to develop a user centric smart city app to summarize complex street level pollution data & health risk information.",
           "Mobile app aims to empower people affected by COPD or asthma to reduce individual pollution exposure in their daily life and provide personalized analytics.",
@@ -57,7 +53,6 @@ const RESUME = [
         sub_header: "Chronicle Books",
         title: "Industrial Design Fellow",
         period: "2018 - 2019",
-        collapsible: true,
         description: [
           "Worked across different publishing-groups and cross functional teams to develop novel products and packagings from conception to market. Considered point of purchase, perceived value, shipping and cost.",
           "Created low fidelity physical prototypes and visual renderings of new concepts. Taught other designers how to utilize 3D modeling and rendering softwares and programs to realize concepts.",
@@ -126,16 +121,23 @@ const RESUME = [
         sub_header: "Reclaim Foundation Community App",
         title: "Reclaim Foundataion",
         period: "2021",
-        collapsible: true,
         description: [
-          "Work with the founding team of non-profit organization Reclaim Foundation",
+          "Build React component from ground up for the front end team to use in the internal community portal.",
+        ],
+      },
+      {
+        sub_header: "Sundial aiir quality monitor app",
+        title: "Sundial",
+        period: "2020",
+        description: [
+          "Develop user journeys to help decide on front-end framework",
+          "Design mobile web app for users to easily navigate and understnad complex air quality data.",
         ],
       },
       {
         sub_header: "Design Fellowship Recruitment Campaign",
         title: "Chronicle Books",
         period: "2018",
-        collapsible: true,
         description: [
           "Designed a poster and interactive postcard inspired by Chronicle Books’ logo, the spectacles. Provided art direction and designed additional campaign materials such as social media assets, website landing page, and email banner to advertise their fellowship program.",
         ],
@@ -144,20 +146,11 @@ const RESUME = [
         sub_header: "Gap UrbanLite Arox Bra",
         title: "MIT and Gap Inc",
         period: "MIT and Gap Inc",
-        collapsible: true,
         description: [
           "Collaborated with an interdisciplinary team at MIT to develop a user friendly, versatile, comfortable day-tonight bra that allows women to transition seamlessly between different activities.",
           "Performed user research to discover pain point of currently available sports bra. Created high fidelity prototype and conducted user testing to refine solution.",
         ],
       },
-      {
-        sub_header: "First Responder Headlamp",
-        title: "Brown University and RI Hospital",
-        period: "Fall 2017",
-        collapsible: true,
-        description: [
-          "Partnered with RI hospital EMT team and Brown University engineers to develop and design a headlamp to ease airway examination during emergency situations. Headlamp aligns light with vision to improve illumination of mouth and airway.",
-          "Conducted qualitative user research to discover challenges of lighting equipment. Created a functional, high fidelity prototype that received positive reviews during final user testing.Conducted qualitative user research to discover challenges of lighting equipment. Created a functional, high fidelity prototype that received positive reviews during final user testing.",
         ],
       },
     ],
