5a48840cbf45fd943a201245203a68257adf213a
Philip-Lau <philip-Lau@noreply.github.com>
2021-09-06 13:56:33 -0700

    Update FourOhFour.js

--- src/containers/FourOhFour.js
+++ src/containers/FourOhFour.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
+import Button from "../components/Button";
+
 const FourOhFour = () => {
+  const [photo, setPhoto] = useState();
+  function handleUpdatePhoto() {
+    axios.get("https://randomfox.ca/floof").then((res) => {
+      setPhoto(res.data.image);
+    });
+  }
+  useEffect(() => handleUpdatePhoto(), []);
+
   return (
     <section className="four-oh-four">
       <h1>AIYA</h1>
       <h3>Seems like you are lost.</h3>
-      <img src='https://source.unsplash.com/600x600?cute-animals' alt='Cute Animals' />
+      <img
+        src={photo}
+        alt="Cute Animals"
+      />
       <p>here are some cute animals for your troubles</p>
       <div>
-      <a href='http://philarty.io/#/404' className='btn btn-lg btn-solid-red' >
-        I want more cutness
-      </a>
-      <Link to="/" className='btn btn-lg btn-solid-green'>
-        Go back to Portfolio
-      </Link>
-
+        <Button onClick={() => handleUpdatePhoto()}>I want more cutness</Button>
+        <Link to="/" className="btn btn-lg btn-solid-green">
+          Go back to Portfolio
+        </Link>
       </div>
     </section>
   );
 };
- 
+
 export default FourOhFour;
